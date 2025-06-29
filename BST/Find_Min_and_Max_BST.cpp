e52a444987e0fc0f82783724e3817fca894c7226
Omar Adly <omarelgaafaryy@gmail.com>
2025-06-29 14:37:54 +0300

    Added recursive methods

--- BST/Find_Min_and_Max_BST.cpp
+++ BST/Find_Min_and_Max_BST.cpp
@@ -1,3 +1,15 @@
+Node* find_min(Node* root)
+{
+    if (root->left == nullptr) return root;
+    return find_min(root->left);
+}
+
+Node* find_max(Node* root)
+ {
+     if (root->right == nullptr) return root;
+     return find_max(root->right);
+ }
+
 int find_min()
     {
         if (root == nullptr) return -1;
